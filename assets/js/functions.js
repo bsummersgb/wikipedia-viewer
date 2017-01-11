491fe1edbc6144f3b33b58165481ddddc5764119
Bryan Summers <bsummersgb@gmail.com>
2017-01-11 21:00:10 +0800

    make call to the media wiki api

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,29 @@
 $( document ).ready(function() {
 
+  $('input[type="submit"]').on('click', function() {
+    var keyword = $('#search-query').val();
+    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + keyword + "&format=json";
+
+    $.ajax({
+      url: url,
+      dataType: 'jsonp',
+      success: function(data) {
+        $('.test').html(data[1][2], data[1][1], data[1][2]); // iterate through with loop
+
+
+
+
+      },
+      error: function() {
+        $('.test').html("You aint got no results");
+
+      }
+    })
+
+  });
+
+
+
 
 
 });
