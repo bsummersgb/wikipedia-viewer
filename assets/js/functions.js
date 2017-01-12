7e548d1ee1a5feacfac143dfe1743f03b0f83392
Bryan Summers <bsummersgb@gmail.com>
2017-01-12 22:30:10 +0800

    add mustache template to html

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,13 +3,14 @@ $( document ).ready(function() {
   $('input[type="submit"]').on('click', function() {
     var keyword = $('#search-query').val();
     var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + keyword + "&format=json";
+    var $ul = $('ul');
 
     $.ajax({
       url: url,
       dataType: 'jsonp',
       success: function(data) {
         for (var i = 0; i < data[1].length; i++) {
-        $('ul').append("<li>" + data[1][i] + " : " + data[2][i]+"</li>");
+          $ul.append("<li>" + data[1][i] + " : " + data[2][i]+"</li>"); // cache the ul and anything else at the top. Then use mustache templating to make it pretty
         }
       },
       error: function() {
